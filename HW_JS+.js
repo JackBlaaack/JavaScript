8253b0b895ddd50020c1d0a57c233955e8a4a3bb
JackBlaaack <106589361+JackBlaaack@users.noreply.github.com>
2022-06-16 14:32:56 +0300

    Update HW_JS+.js
    change the file

--- HW_JS+.js
+++ HW_JS+.js
@@ -1,22 +1,24 @@
-function checkAge(age_1) {
-age_1 = Number(age_1);
-console.log(typeof(age_1));
-if(typeof(age_1) =="number"){
- if(age_1<18){
-        console.log("go home");
-     }
-    
-     else if(age_1 >=  18 && age_1 <  61){
-        console.log("Welcome");
-     }
-    
-     else if(age_1 >61){
-        console.log("Keep calm and look Culture channel");
-     }else{
-        console.log("Technical work");
-     }
-}else {
-    console.log("Error");
+function checkAge(age_1){
+    age_1 = Number(age_1);
+    console.log(typeof(age_1));
+     if(typeof(age_1)=="number"){
+        if(typeof(age_1=="NaN")){
+            console.log("Error");
+        }
+
+    if(age_1 < 18){
+    alert("Go home");
+    }
+    else if(age_1 >=18 && age_1 <= 61){
+        alert("Welcome");
+    }
+    else if (age_1 > 61){
+        alert("Keep calm and look Culture channel");
+    }
+    }
+    else {
+        alert("Error");
+    }
 }
-}
-checkAge('17');
+checkAge();
+console.log(checkAge(prompt('Enter your age','')));
