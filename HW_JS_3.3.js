6a4dd5c53afe8b8f62ab43d98419734b4844cd58
JackBlaaack <106589361+JackBlaaack@users.noreply.github.com>
2022-07-19 23:53:58 +0300

    Update HW_JS_3.3.js
    Change  the info in the file HW_JS_3.3.js

--- HW_JS_3.3.js
+++ HW_JS_3.3.js
@@ -53,72 +53,57 @@ const enterprises = [
     ]
   }
 ];
-// for( i=0; i<enterprises.length; i++ ) {
-//   let sum = 0;
-//   for( j=0; j<enterprises[i].departments.length; j++ ) {
-//     sum += enterprises[i].departments[j].employees_count;   
-//     }     
-//     console.log(`${enterprises[i].name}  (${sum} сотрудников) `);
+for( i=0; i<enterprises.length; i++ ) {
+  let sum = 0;
+  for( j=0; j<enterprises[i].departments.length; j++ ) {
+    sum += enterprises[i].departments[j].employees_count;   
+    }     
+    console.log(`${enterprises[i].name}  (${sum} сотрудников) `);
     
-//     for( j= 0; j< enterprises[i].departments.length; j++ ) {
+    for( j= 0; j< enterprises[i].departments.length; j++ ) {
 
-//       console.log(`-${enterprises[i].departments[j].name}  (${enterprises[i].departments[j].employees_count} сотрудников) `);
-//     }
-//   }
-
-// function getEnterpriseName(element) {
-//   for (i =0 ; i< enterprises.length ; i++) {
-//     for (j = 0 ; j <enterprises[i].departments; j++) {
-//       if (element === enterprises[i].departments[j].name || element === enterprises[i].id) {
-//        console.log(enterprises[i].name);
-//        break;
-     
-// } else if (element === enterprises[i].name){
-//   console.log('Это не департамент');
-//   break;
-// }
-//  }
-
-// }
-// }
-// getEnterpriseName('Отдел Маркетинга');
-// console.log()
-// function addEnterprise(newName) {
-//  length = enterprises.length;
-// let lastAdd = enterprises[length - 1].departments.find(element => element.id);
-// let lastId = lastAdd.id ;
-//  let newObject = {
-//   id: lastId +1,
-//   name: newName,
-//  departments: [],
-// };
-//   enterprises.push({
-//     id: lastId+1,
-//     name: newName,
-//    departments: [
-//     {
-//       id: lastId+1,
-//       name: 'Отдел безделия',
-//       employees_count: 0,
-//     }
-//    ],
-// });
-// }
-// addEnterprise('Предприятие 4')
-// console.log(enterprises);
-
-function addDepartment(idItem, newDep) {
-for (let i =0; i <enterprises.length; i++) {
-  for(let j=0; j< enterprises[i].departments.length; j++) {
-if (idItem === enterprises[i].id) {
-  enterprises[i].departments.push({
-    id: idItem,
-    name: newDep,
-    employees_count: 0,
-  })
-}
+      console.log(`-${enterprises[i].departments[j].name}  (${enterprises[i].departments[j].employees_count} сотрудников) `);
+    }
   }
+
+function getEnterpriseName(element) {
+  for (i =0 ; i< enterprises.length ; i++) {
+    for (j = 0 ; j <enterprises[i].departments; j++) {
+      if (element === enterprises[i].departments[j].name || element === enterprises[i].id) {
+       console.log(enterprises[i].name);
+       break;
+     
+} else if (element === enterprises[i].name){
+  console.log('Это не департамент');
+  break;
+}
+ }
+
 }
 }
-addDepartment(1, 'хей');
+getEnterpriseName('Отдел Маркетинга');
+console.log()
+function addEnterprise(newName) {
+ length = enterprises.length;
+let lastAdd = enterprises[length - 1].departments.find(element => element.id);
+let lastId = lastAdd.id ;
+ let newObject = {
+  id: lastId +1,
+  name: newName,
+ departments: [],
+};
+  enterprises.push({
+    id: lastId+1,
+    name: newName,
+   departments: [
+    {
+      id: lastId+1,
+      name: 'Отдел безделия',
+      employees_count: 0,
+    }
+   ],
+});
+}
+addEnterprise('Предприятие 4')
 console.log(enterprises);
+
