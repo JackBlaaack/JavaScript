7f845a21bf2ec82e721679cadfff4a825b77b840
JackBlaaack <106589361+JackBlaaack@users.noreply.github.com>
2022-08-01 00:00:44 +0300

    Add files via upload
    Add the new file EmailGenerator.js

---
+++ EmailGenerator.js
@@ -0,0 +1,15 @@
+ getEmail = length => {
+   
+    let symbols = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','0','1','2','3','4','5','6','7','8','9'];
+let email = '';
+   
+    for(let el = 0; el < length; ++el){
+      email += symbols[
+           Math.floor(
+               Math.random() * symbols.length
+          )
+      ];
+    }
+    return email;
+}
+console.log(getEmail(6)+'@gmail.com');
